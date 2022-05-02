55485003c375adebaa6a77d6a96e4fa1da80620c
anderson <anderson.ap133@gmail.com>
2022-05-01 23:57:20 -0300

    Created Theme Storage Hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import React, {useState} from 'react';
+import React from 'react';
 import GlobalStyle from './styles/global';
 import { ThemeProvider } from 'styled-components';
 import light from './styles/themes/light';
 import dark from './styles/themes/dark';
+import usePersistedState from './utils/usePersistedState'
 import Header from './components/header/header';
 import { Navbar } from './components/navbar';
 
 function App() {
 
-  const [theme, setTheme] = useState(light);
+  const [theme, setTheme] = usePersistedState('theme', light);
 
   const toggleTheme = () => {
     setTheme(theme.title === 'light'? dark : light);
--- src/styles/themes/dark.ts
+++ src/styles/themes/dark.ts
@@ -2,8 +2,8 @@ export default {
     title: 'dark',
 
     colors: {
-        primary: '#0892A5',
-        secundary: '#0CA4A5',
+        primary: '#333',
+        secundary: '#c62e65',
 
         background: '#222',
         text: '#fff'
