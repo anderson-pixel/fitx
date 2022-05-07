d8e41721bedc0c4f9de63d3d6297b934ccd08eae
anderson <anderson.ap133@gmail.com>
2022-05-06 23:01:54 -0300

    Updated Team Section

--- src/components/header/Team/team.tsx
+++ src/components/header/Team/team.tsx
@@ -1,14 +1,68 @@
 import React from 'react'
 import styled from 'styled-components'
+import catimg from '../../../images/catshot.png'
 
 const TeamContainer = styled.section`
 width: 100%;
 height: 600px;
+
+.box{
+      height: 50rem;
+      overflow: hidden;
+      position: relative;
+
+      &:hover {
+          .content {
+              left:-100%;
+          }
+
+          .share {
+              left: 0;
+          }
+      }
+
+      img{
+        height: 100%;
+        width: 100%;
+        object-fit: cover;
+      }
+
+      .content {
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        background: white;
+        padding: 1rem 2rem;
+
+        span {
+          font-size: 1.7rem;
+          color: red;
+        }
+
+        h3{
+          font-size:2rem;
+          color: white;
+        }
+      }
+      .share {
+          position: absolute;
+          bottom: 0;
+          left: 0;
+          background: white;
+          padding: 1rem;
+
+          a {
+              font-size: 2rem;
+              color: black;
+              margin: 0 1rem;
+          }
+      }
+}
 `
 
 function Team() {
   return (
-    <TeamContainer id="team">
+    <TeamContainer id="team" className="team">
         <div className="heading">
             <span>Team</span>
             <h3>placeholder</h3>
@@ -17,7 +71,7 @@ function Team() {
         <div className="box-container container">
 
             <div className="box">
-                <img src="" alt="placeholder"></img>
+                <img src={catimg} alt="placeholder"></img>
                 <div className="content">
                     <span>placeholder</span>
                     <h3>placeholder</h3>
@@ -31,7 +85,7 @@ function Team() {
             </div>
 
             <div className="box">
-                <img src="" alt="placeholder"></img>
+                <img src={catimg} alt="placeholder"></img>
                 <div className="content">
                     <span>placeholder</span>
                     <h3>placeholder</h3>
@@ -45,7 +99,7 @@ function Team() {
             </div>
 
             <div className="box">
-                <img src="" alt="placeholder"></img>
+                <img src={catimg} alt="placeholder"></img>
                 <div className="content">
                     <span>placeholder</span>
                     <h3>placeholder</h3>
