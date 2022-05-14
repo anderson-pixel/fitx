85a517a89626f0f569732ac73c78c1339efc275d
anderson <anderson.ap133@gmail.com>
2022-05-13 23:45:16 -0300

    Changed Feature Images

--- src/components/header/About/about.tsx
+++ src/components/header/About/about.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCheck,faCheckSquare } from '@fortawesome/free-solid-svg-icons';
+import {faCheckSquare } from '@fortawesome/free-solid-svg-icons';
 import aboutimg from '../../../images/fitx1.png'
 
 const AboutContainer = styled.section`
@@ -21,6 +21,16 @@ function About() {
             <h3><span>about us</span></h3>
             <h1>Daily Workout And Stay Active at Home</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio exercitationem enim impedit vitae dignissimos, deleniti omnis pariatur eum fugit libero, distinctio tempora voluptatem reiciendis, voluptate saepe corporis unde illo repellendus.</p>
+            <ul>
+              <li> <i>
+       <FontAwesomeIcon icon={faCheckSquare} size="1x"/> </i>Lorem ipsum dolor ehenderit deleniti dolore doloribus tempora numquam, ab fugit!</li>
+       <li> <i>
+       <FontAwesomeIcon icon={faCheckSquare} size="1x"/> </i>Lorem ipsum dolor ehenderit deleniti dolore doloribus tempora numquam, ab fugit!</li>
+       <li> <i>
+       <FontAwesomeIcon icon={faCheckSquare} size="1x"/> </i>Lorem ipsum dolor ehenderit deleniti dolore doloribus tempora numquam, ab fugit!</li>
+       <li> <i>
+       <FontAwesomeIcon icon={faCheckSquare} size="1x"/> </i>Lorem ipsum dolor ehenderit deleniti dolore doloribus tempora numquam, ab fugit!</li>
+            </ul>
           </div>
 
     </AboutContainer>
