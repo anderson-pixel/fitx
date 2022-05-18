b2f6ba94482a2659baa8ee5bd9f51c05fe61d204
anderson <anderson.ap133@gmail.com>
2022-05-18 20:15:26 -0300

    Finished Home Section

--- src/components/header/Home/home.tsx
+++ src/components/header/Home/home.tsx
@@ -2,14 +2,17 @@ import React from 'react';
 import styled from 'styled-components';
 import imghome from '../../../images/img6.jpg'
 
-const HomeContainer = styled.section`
-padding: 0;
-height: 625px;
+const HomeBg = styled.div`
 background: url(${imghome}), linear-gradient(rgba(0,0,0,.8), rgba(0,0,0,.8));
 background-size: cover;
 background-position: center;
 background-blend-mode: multiply;
 background-attachment: fixed;
+`
+
+const HomeContainer = styled.section`
+padding: 0;
+height: 625px;
 text-align: center;
 align-items: center;
 display: flex;
@@ -69,6 +72,7 @@ align-items: center;
 
 const Home = () => {
   return (
+    <HomeBg className="home-bg">
     <HomeContainer id="home">
      <div className="home-slider">
      <div className="wrapper">
@@ -87,6 +91,7 @@ Training with Certified Fitx Coaches!</h4>
      </div>
     </div>
     </HomeContainer>
+    </HomeBg>
   )
 }
 
